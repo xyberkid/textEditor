f17261e47ce2d11db161984e6211ed1206603cd6
Abraham MB Selebay <110640726+xyberkid@users.noreply.github.com>
2022-12-24 11:28:02 -0400

    Add files via upload

---
+++ Assign/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEditor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rtbEditor = New System.Windows.Forms.RichTextBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'rtbEditor
+        '
+        Me.rtbEditor.Location = New System.Drawing.Point(0, 0)
+        Me.rtbEditor.Name = "rtbEditor"
+        Me.rtbEditor.Size = New System.Drawing.Size(398, 227)
+        Me.rtbEditor.TabIndex = 0
+        Me.rtbEditor.Text = ""
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(404, 12)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "Save File"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(404, 52)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpen.TabIndex = 2
+        Me.btnOpen.Text = "Open File"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(404, 92)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 3
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'frmEditor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(491, 248)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.rtbEditor)
+        Me.Name = "frmEditor"
+        Me.Text = "Text Editor"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents rtbEditor As System.Windows.Forms.RichTextBox
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnOpen As System.Windows.Forms.Button
+    Friend WithEvents btnClose As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ Assign/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.IO
+Public Class frmEditor
+    Public writefile As StreamWriter
+    Public readfile As StreamReader
+    Dim result As DialogResult
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+        SaveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        result = SaveFileDialog1.ShowDialog
+        If result = DialogResult.OK Then
+            writefile = New StreamWriter(SaveFileDialog1.FileName, False)
+            writefile.Write(rtbEditor.Text)
+            writefile.Close()
+            rtbEditor.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnOpen_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOpen.Click
+        OpenFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        result = OpenFileDialog1.ShowDialog
+        If result = DialogResult.OK Then
+            readfile = New StreamReader(OpenFileDialog1.FileName)
+            rtbEditor.Text = readfile.ReadToEnd()
+            readfile.Close()
+        End If
+    End Sub
+    Private Sub btnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+        End
+    End Sub
+End Class
